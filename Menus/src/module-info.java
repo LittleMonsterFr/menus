c8ae334cde565743069a167291e0b69e11a4ad33
Damien Laidin <damien@chirp.io>
2019-01-04 10:10:52 +0000

    uncommented module info content

--- Menus/src/module-info.java
+++ Menus/src/module-info.java
@@ -1,12 +1,12 @@
-//module Menus {
-//    requires javafx.controls;
-//    requires javafx.fxml;
-//    requires java.desktop;
-//
-//    opens code.menu.application to javafx.fxml;
-//    opens code.menu.loader to javafx.fxml;
-//    opens code.menu.plat to javafx.fxml;
-//    exports code.menu.application;
-//    exports code.menu.loader;
-//    exports code.menu.plat;
-//}
+module Menus {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.desktop;
+
+    opens code.menu.application to javafx.fxml;
+    opens code.menu.loader to javafx.fxml;
+    opens code.menu.plat to javafx.fxml;
+    exports code.menu.application;
+    exports code.menu.loader;
+    exports code.menu.plat;
+}
